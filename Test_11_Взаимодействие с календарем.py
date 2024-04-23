a3f92df015619bf2eb39e888996faa4d6d690676
Viktor Matuizo <99718345+Vitek169@users.noreply.github.com>
2024-04-23 15:28:48 +0300

    обновил тест с каледарем

--- Test_11_Взаимодействие с календарем.py
+++ Test_11_Взаимодействие с календарем.py
@@ -17,13 +17,19 @@ driver.maximize_window()
 time.sleep(1)
 
 new_date = driver.find_element(By.XPATH, '//input[@id="datePickerMonthYearInput"]')
-new_date.send_keys(Keys.CONTROL + 'a') # Выделяем текст в поле
-new_date.send_keys(Keys.BACKSPACE) # Одним нажатием удаляем выделенный текст в поле
+# new_date.send_keys(Keys.CONTROL + 'a') # Выделяем текст в поле
+# new_date.send_keys(Keys.BACKSPACE) # Одним нажатием удаляем выделенный текст в поле
+#
+# time.sleep(1)
+# new_date.send_keys('06/16/2024')
+# time.sleep(1)
+# new_date.send_keys(Keys.RETURN)
 
+new_date.click()
 time.sleep(1)
-new_date.send_keys('06/16/2024')
-time.sleep(1)
-new_date.send_keys(Keys.RETURN)
+date_23 = driver.find_element(By.XPATH, '//div[contains(@class,"react-datepicker__day--today")]') # Выбор части необходимого селектора, заключения его в контейнер, для того, что бы не привязываться к конкретной дате на календаре и выборе даты сегодняшнего дня
+date_23.click()
+time.sleep(2)
 
 
 
