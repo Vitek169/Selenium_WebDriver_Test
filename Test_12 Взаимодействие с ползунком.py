4fc8f585608588b69a437676a73367f76a959933
Viktor Matuizo <99718345+Vitek169@users.noreply.github.com>
2024-04-23 16:05:57 +0300

    Исправил тест с 12

--- Test_12 Взаимодействие с ползунком.py
+++ Test_12 Взаимодействие с ползунком.py
@@ -2,6 +2,7 @@ import datetime
 import time
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -14,29 +15,19 @@ base_url = 'https://html5css.ru/howto/howto_js_rangeslider.php'
 driver.get(base_url)
 driver.maximize_window()
 
-login_standard_user = 'standard_user'
-password_all = 'secret_sauce'
+action = ActionChains(driver)
 
-user_name = driver.find_element(By.XPATH, "//input[@id ='user-name']")
-user_name.send_keys(login_standard_user)
-print('Input Login')
-password = driver.find_element(By.XPATH, "//input[@id ='password']")
-password.send_keys(password_all)
-print('Input password')
-button_login = driver.find_element(By.XPATH, "//input[@id ='login-button']")
-button_login.click()
-print('Click Logon Button')
-# text_products = driver.find_element(By.XPATH, "//span[@class='title']")
-# value_text_ptoducts = text_products.text
-# print(value_text_ptoducts)
-# assert value_text_ptoducts == 'PRODUCTS'
-# print('Good')
-now_date = datetime.datetime.utcnow().strftime('%Y.%m.%d.%H.%M.%S')# Возвращает дату. Задает Годд. Месяц. День. Час. Минуты. Секунд.
-name_screenshot = 'screenshot' + now_date + '.png'
-driver.save_screenshot('D:\\QA\\Dev_Selenium_Python\\Screen\\' + name_screenshot) #Скриншот экрана
+filter_def = driver.find_element(By.XPATH, '//input[@type="range"]')
 
-url = 'https://www.saucedemo.com/inventory.html'
-get_url = driver.current_url
-print(get_url)
-assert url == get_url
-print('Good URL')
+
+"""метод кликнуть и тянуть ползунок click_and_hold() и метод перемещение по горизонтали и вертикали move_by_offset(), после чего метод отпускания мыши release() и метод сохранения резульата perform()"""
+action.click_and_hold(filter_def).move_by_offset(-30, 0).release().perform()
+print('Бегунок сдвинулся')
+
+time.sleep(1)
+driver.close()
+
+
+
+
+
