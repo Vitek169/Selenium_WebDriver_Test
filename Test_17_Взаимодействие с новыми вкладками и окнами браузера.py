ab7b207f2f8442001dac274ac1c458a4065b1198
Viktor Matuizo <99718345+Vitek169@users.noreply.github.com>
2024-05-09 21:43:19 +0300

    Добавил мктоды переключения между окнами, а так же добавил метоты Alertдля всплывающих окон

---
+++ Test_17_Взаимодействие с новыми вкладками и окнами браузера.py
@@ -0,0 +1,54 @@
+import datetime
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get('https://demoqa.com/browser-windows')
+driver.maximize_window()
+
+
+"""Переключение между вкладками"""
+# new_tab_button = driver.find_element(By.XPATH, "//button[@id='tabButton']")
+# new_tab_button.click()
+# print("Click Tab URL", driver.current_url, sep='\n')
+# heder_1 = driver.find_element(By.XPATH, '//h1[@class="text-center"]')
+# print(heder_1.text)
+#
+# driver.switch_to.window(driver.window_handles[1])
+# print(driver.current_url)
+#
+# heder_2 = driver.find_element(By.XPATH, '//h1[@id="sampleHeading"]')
+# print(heder_2.text)
+#
+# driver.switch_to.window(driver.window_handles[1])
+# print(driver.current_url)
+
+"""Взаимодействие с новым окном переключение"""
+
+new_windows_button = driver.find_element(By.XPATH, '//button[@id="windowButton"]')
+new_windows_button.click()
+heder_1 = driver.find_element(By.XPATH, '//h1[@class="text-center"]')
+print("Click New Window", driver.current_url, heder_1.text, sep='\n')
+
+windows_1 = driver.window_handles[0]
+windows_2 = driver.window_handles[1]
+
+driver.switch_to.window(windows_2)
+print(driver.current_url)
+
+heder_2 = driver.find_element(By.XPATH, '//h1[@id="sampleHeading"]')
+print(heder_2.text)
+
+
+driver.switch_to.window(windows_1)
+print(driver.current_url, heder_1.text, sep='\n')
+
+time.sleep(5)
+driver.close()
